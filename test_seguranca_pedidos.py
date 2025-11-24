7c2303d2b08150a9fb1e37b199c902c1b996d3e3
QABiscoitoGatinho2025 <qabiscoitogatinho@gmail.com>
2025-11-24 23:07:30 +0000

    alteração 2

--- test_seguranca_pedidos.py
+++ test_seguranca_pedidos.py
@@ -2,8 +2,10 @@
 import pytest
 from sistema_pedidos import buscar_detalhes_pedido
 
-def test_usuario_nao_pode_acessar_pedido_de_outro_usuario(id_pedido: int, id_usuario_atacante: int):
+def test_usuario_nao_pode_acessar_pedido_de_outro_usuario():
 
+    id_pedido = 101
+    id_usuario_atacante = 2
     resultado = buscar_detalhes_pedido(id_pedido, id_usuario_atacante)
 
     assert resultado == {}, (
